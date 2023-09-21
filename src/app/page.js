247237c86a01613e5a05f7baa1128a97853d95e9
aesthetic <15858616+aesthetic0001@users.noreply.github.com>
2023-09-20 20:26:05 -0400

    added autocomplete

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 "use client";
 import React from 'react';
 import Image from 'next/image'
+import Script from 'next/script'
 import referenceIDs from '../constants/search_ids.json'
+import {compareTwoStrings} from "string-similarity";
 
 export default function Home() {
     const [filter, setFilter] = React.useState(null);
+    const [suggestions, setSuggestions] = React.useState([]);
 
     const handleUpload = (event) => {
         const file = event.target.files[0]
@@ -28,8 +31,13 @@ export default function Home() {
     }
 
     const handleSearch = (event) => {
-        const search = event.target.value;
-
+        const search = event.target.value.toLowerCase()
+        if (!search) return setSuggestions([])
+        // find 5 suggestions and list them in terms of relevance
+        const suggestions = Object.keys(referenceIDs).map((name) => {
+            return {name, id: referenceIDs[name], rating: compareTwoStrings(search, name)}
+        }).sort((a, b) => b.rating - a.rating).slice(0, 5)
+        setSuggestions(suggestions)
     }
 
     return (
@@ -54,6 +62,22 @@ export default function Home() {
                            className="block w-1/4 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline-none transition-all ease-in-out hover:w-1/2 duration-200"
                            placeholder="Search for an item to use in your filter" required onChange={handleSearch}>
                     </input>
+                    {/* handle suggestions /*/}
+                    {
+                        suggestions.length > 0 ?
+                            <div className="absolute z-10 w-1/4 mt-2 bg-white rounded-md shadow-lg dark:bg-gray-800">
+                                <ul className="overflow-hidden text-sm text-gray-700 divide-y divide-gray-200 rounded-md shadow-lg dark:text-gray-400 dark:divide-gray-700">
+                                    {suggestions.map((suggestion, index) => {
+                                        return <li key={index} className="relative">
+                                            <button type="button"
+                                                    className="flex items-center w-full px-2 py-2 text-left hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-700">
+                                                <span className="ml-2">{suggestion.name}</span>
+                                            </button>
+                                        </li>
+                                    })}
+                                </ul>
+                            </div> : null
+                    }
                     <label htmlFor="file-upload"
                            className="absolute top-0 right-8 rounded bg-emerald-800 py-3 px-5 transition-all ease-in-out hover:bg-emerald-700 hover:scale-105 duration-100">
                         <h1>Upload Filter</h1>
